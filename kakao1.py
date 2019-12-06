b46c9c96946693249f68f3e916ecba3b3395a1ad
CHAEUN PARK <codjs0910@naver.com>
2019-12-07 01:31:08 +0900

    kakao2

--- kakao1.py
+++ kakao1.py
@@ -20,6 +20,23 @@ def solution(record):
         answer.append(result)
     return answer
 
+idDict = dict()
+def solution_else(record):
+    answer = []
+    logList = []
+    for e in record:
+        dataList = e.split(" ")
+        if dataList[0] == "Leave":
+            logList.append([dataList[1], "님이 나갔습니다."])
+        elif dataList[0] == "Enter":
+            idDict[dataList[1]] = dataList[2]
+            logList.append([dataList[1], "님이 들어왔습니다."])
+        elif dataList[0] == "Change":
+            idDict[dataList[1]] = dataList[2]
+    print(logList)
+    for log in logList:
+        answer.append(idDict[log[0]] + log[1])
+    return answer
 record = [
     "Enter uid1234 Muzi"
     , "Enter uid4567 Prodo"
@@ -29,6 +46,9 @@ record = [
 ]
 
 answer = solution(record)
-
+answer_else = solution_else(record)
 for ans in answer:
+    print(ans)
+
+for ans in answer_else:
     print(ans)
